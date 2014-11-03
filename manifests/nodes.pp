98d21e7c111e51f0f3ec676e4870a9a10d56399b
Rahul Janghel <rahul.janghel@gmail.com>
2014-11-03 16:35:22 +0530

    Updated nodes file.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,1 +1,35 @@
-  include zc
+class zc {
+        package {'zabbix-agent':
+        ensure => removed,
+        }
+}
+
+augeas { 'Set Zabbix Client config':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set Server 192.168.56.102'],
+        }
+
+augeas { 'Set Hostname':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set Hostname localhost'],
+        }
+
+augeas { 'Set PID':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set PidFile /var/run/zabbix/zabbix_agentd.pid'],
+        }
+
+augeas { 'Set LogFile':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set LogFile /var/log/zabbix/zabbix_agentd.log'],
+        }
+
+augeas { 'Set LogFileSize':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set LogFileSize 0'],
+        }
+
+augeas { 'Set ServerActive':
+context => '/files/etc/zabbix/zabbix_agentd.conf',
+changes => ['set ServerActive 127.0.0.1'],
+        }
